f78bab28c35a7dea914d062c6a44ec2e444a8bc6
kwooS <kwsong0725@gmail.com>
2023-11-15 13:55:56 +0900

    practice added

--- Assets/Scripts/TestManager.cs
+++ Assets/Scripts/TestManager.cs
@@ -26,17 +26,38 @@ public class TestManager : MonoBehaviour
 
         string orderStr = "";
         string digitStr = "";
+        string practiceStr = "";
+
+        for(int i = 0; i < userNo+1; i++)
+        {
+            orderStr = orderSR.ReadLine();
+        }
+        orderSR.Close();
 
         for (int i = 0; i < userNo*4+trialNo + 1; i++)
         {
-            orderStr = orderSR.ReadLine();
             digitStr = digitSR.ReadLine();
+        }
+        digitSR.Close();
+
+        if (isPractice)
+        {
+            StreamReader practiceSR = new StreamReader(digitDir);
+            for (int i = 0; i < practiceIdx + 1; i++)
+            {
+                practiceStr = practiceSR.ReadLine();
+            }
+
+            practiceDigits = practiceStr.Split(',')[0..3];
+            practiceSR.Close();
         }
 
         Debug.Log(orderStr);
         Debug.Log(digitStr);
 
         order = orderStr.Split(';')[trialNo].Split(",");
+        order[0] = order[0][1..];
+        order[1] = order[1][..^1];
         digits = digitStr.Split(',')[0..14];
     }
 
@@ -44,8 +65,5 @@ public class TestManager : MonoBehaviour
     void Update()
     {
 
-    }
-    public void loadData()
-    {
     }
 }
